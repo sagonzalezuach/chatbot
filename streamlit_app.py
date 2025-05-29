dedc6044cbeb59f1e4977af8c24f8706b6e4480d
sagonzalezuach <sagonzalez@uach.mx>
2025-05-29 16:02:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ openai_api_key = st.secrets["api_key"]
 # Create an OpenAI client.
 client = OpenAI(api_key=openai_api_key)
 
-prompt = st.chat_input("Cómo puedo ayudarte hoy?")
+prompt = st.chat_input("¿Cómo puedo ayudarte hoy?")
 #if prompt==None:
 #   st.stop()
 
